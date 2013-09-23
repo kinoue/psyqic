7e0b80e2e8ab05b50343f93b58e201c3cb3f54f6
keisukeinoue <kinoue@syr.edu>
2013-09-23 12:17:15 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Psyqic::Application.config.secret_token = '1466d7056a91001287da8f5aaab201e8188fed3a6525ff35e02f631ddcd7d9ad3012599ee2986e253cf5b5f50e72f9d59daea7a6b99f94348c3d08e2bd3f2034'
